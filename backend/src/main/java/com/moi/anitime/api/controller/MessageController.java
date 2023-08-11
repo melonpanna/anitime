d588918e445894ef4519d134308d34ffa45171b7
김민태 <kimmintae981019@gmail.com>
2023-08-11 17:39:06 +0900

    Feat: RabbitMq 설정 및 Message 로직

--- backend/src/main/java/com/moi/anitime/api/controller/MessageController.java
+++ backend/src/main/java/com/moi/anitime/api/controller/MessageController.java
@@ -5,6 +5,8 @@ import com.moi.anitime.api.response.chat.ChatRes;
 import com.moi.anitime.model.entity.chat.ChatMessage;
 import com.moi.anitime.model.service.chat.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,16 @@ import java.sql.SQLException;
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
-    private final SimpMessagingTemplate smso;
+    private final RabbitTemplate rabbitTemplate;
     private final ChatService chatService;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String route_key;
+
     @MessageMapping("/message")
     public void message(ChatMessageReq message) {
         ChatRes chat = chatService.sendChat(message);
-        smso.convertAndSend("/sub/message/"+message.getRoomNo(), chat);
+        rabbitTemplate.convertAndSend(exchange, "/sub/message/"+message.getRoomNo(), chat);
     }
 }
