2cd2f4d525a7a8e3cda1635355cbba7c0b1b6ef4
김민태 <kimmintae981019@gmail.com>
2023-08-14 12:28:26 +0900

    Update WebSocketConfig.java

--- backend/src/main/java/com/moi/anitime/config/WebSocketConfig.java
+++ backend/src/main/java/com/moi/anitime/config/WebSocketConfig.java
@@ -24,7 +24,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 //        registry.enableSimpleBroker("/sub");
         registry.setPathMatcher(new AntPathMatcher("."));
         registry.setApplicationDestinationPrefixes("/pub");
-        registry.setRelayPort(61613);
         registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
     }
 }
